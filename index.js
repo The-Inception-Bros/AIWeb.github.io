1033e7b91b4d17ad75ad385d6fa831f7e06b25c3
The-Inception-Bros <147085008+The-Inception-Bros@users.noreply.github.com>
2024-06-22 03:33:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function search_list() {
+    let input = document.getElementById('searchbar').value.trim().toLowerCase();
+    let x = document.getElementsByClassName('content');
+  
+    if (input === '') {
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+    } else {
+      for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display = "none";
+        } else {
+          x[i].style.display = "list-item";
+        }
+      }
+    }
+  }
